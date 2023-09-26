a39d9c1e25fd0a56db4f301fde533b5e856fb793
Saurabh Kushwaha <1ms22cs126@msrit.edu>
2023-09-26 13:50:44 +0530

    Updated

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
-import mediapipe as mp
-import cv2 
+import cv2
 import numpy as np
-import uuid
 import os
+from matplotlib import pyplot as plt
+import time
+import mediapipe as mp
 
-mp_drawing = mp.solutions.drawing_utils
-mp_hands = mp.solutions.hands
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
 
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+def draw_landmarks(image, results):
+    mp_drawing.draw_landmarks(image, results.face_landmarks, mp_holistic.FACE_CONNECTIONS) # Draw face connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand 
+
+def draw_styled_landmarks(image, results):
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2))
+     
 cap = cv2.VideoCapture(0)
-
-with mp_hands.Hands(min_detection_confidence=0.6, min_tracking_confidence=0.5, max_num_hands = 2) as hands:
-
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
     while cap.isOpened():
+
+        # Read feed
         ret, frame = cap.read()
-
-        image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        image.flags.writeable = False
-        results = hands.process(image)
-        image.flags.writeable = True
-        image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
-
-        if results.multi_hand_landmarks:
-            for num, hand in enumerate(results.multi_hand_landmarks):
-                mp_drawing.draw_landmarks(image, hand, mp_hands.HAND_CONNECTIONS, 
-                mp_drawing.DrawingSpec(color=(121, 22, 76), thickness=2, circle_radius=4), 
-                mp_drawing.DrawingSpec(color=(121, 44, 250), thickness=2, circle_radius=2))
-
-        cv2.imshow('Hand Tracking', image)
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        # Show to screen
+        cv2.imshow('OpenCV Feed', image)
+        # Break gracefully
         if cv2.waitKey(10) & 0xFF == ord('q'):
             break
-    
+
 cap.release()
+cv2.destroyAllWindows()
+
+def extract_keypoints(results):
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([lh, rh])
